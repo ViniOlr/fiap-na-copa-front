05c6d9b23ae4e495ba660b983dc947b4653e0938
Vinicius Oliveira <Vinicius.Oliveira@taxsolutions.local>
2022-10-21 17:00:57 -0300

    Adicionando o container no login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,21 +5,23 @@ import { Container, SectionLogin } from '../../style/styled';
 const Login = ()=> {
   return (
     <SectionLogin>
-        <div className="formulario">
-            <h2>Olá, Seja Bem Vindo!</h2>
-            <p>Efetue o login em nossa platarforma para ter acesso aos nossos diversos conteudos.</p>
-            <form>
-                <div>
-                    <label>Usuário</label>
-                    <input type="text" name="login" id="login" />
-                </div>
-                <div>
-                    <label>Senha</label>
-                    <input type="password" name="senha" id="senha" />
-                </div>
-                <button>Logar</button>
-            </form>
-        </div>
+        <Container>
+            <div className="formulario">
+                <h2>Olá, Seja Bem Vindo!</h2>
+                <p>Efetue o login em nossa platarforma para ter acesso aos nossos diversos conteudos.</p>
+                <form>
+                    <div>
+                        <label>Usuário</label>
+                        <input type="text" name="login" id="login" />
+                    </div>
+                    <div>
+                        <label>Senha</label>
+                        <input type="password" name="senha" id="senha" />
+                    </div>
+                    <button>Logar</button>
+                </form>
+            </div>
+        </Container>
     </SectionLogin>
   );
 }
